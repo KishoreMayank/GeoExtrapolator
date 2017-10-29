94676c98049597293ad935c8a19b85ced6474e87
Mayank Kishore <mayankkishore.in@gmail.com>
2017-10-29 02:46:15 -0400

    mayank

---
+++ Mayank.js
@@ -0,0 +1,21 @@
+var SanFrancisco = {
+    Climate: "Cloudy",
+    Geography: ["Coastal", "Valley"],
+    Elevation: "Above Sea Level",
+    Population: "Urban",
+    Position: {lat: 1, lng: 1}
+};
+
+var cityName = {
+    Climate: ["string1", "string2"],
+    Geography: ["string1", "string2"],
+    Elevation: ["string1", "string2"],
+    Population: ["string1", "string2"]
+};
+
+var cityName = {
+    Climate: ["string1", "string2"],
+    Geography: ["string1", "string2"],
+    Elevation: ["string1", "string2"],
+    Population: ["string1", "string2"]
+};
