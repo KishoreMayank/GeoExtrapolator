e5f616438f6e015403efe5ace78fe54acc83ee4a
Mayank Kishore <mayankkishore.in@gmail.com>
2017-10-29 04:42:24 -0400

    lol

--- Mayank.js
+++ Mayank.js
@@ -6,16 +6,11 @@ var SanFrancisco = {
     Position: {lat: 1, lng: 1}
 };
 
-var cityName = {
-    Climate: ["string1", "string2"],
-    Geography: ["string1", "string2"],
-    Elevation: ["string1", "string2"],
-    Population: ["string1", "string2"]
-};
 
 var cityName = {
     Climate: ["string1", "string2"],
     Geography: ["string1", "string2"],
     Elevation: ["string1", "string2"],
     Population: ["string1", "string2"]
+    Position: {lat: 1, lng: 1}
 };
