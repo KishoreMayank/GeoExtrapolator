4703094809176206f11ade1c81f445d06365303d
Prajwal-kumar <prkumar@student.edgemont.org>
2017-10-29 04:36:18 -0400

    5

--- script.js
+++ script.js
@@ -10,9 +10,20 @@ function getPreferences() {
 
     if(clim === "Temperate" && geo === "Coastal" && pop === "Urban") {
         locations = [
-            {lat: 47.5622, lng: 13.6493}
+            {lat: 47.5622, lng: 13.6493},
+            {lat: 40.811550, lng: -73.946477},
+            {lat: 40.811550, lng: -73.946477},
+            {lat: -31.563910, lng: 147.154312},
+              {lat: -33.718234, lng: 150.363181},
+              {lat: -33.727111, lng: 150.371124},
+              {lat: -33.848588, lng: 151.209834},
+              {lat: -33.851702, lng: 151.216968},
+              {lat: -34.671264, lng: 150.863657},
+              {lat: -35.304724, lng: 148.662905},
+              {lat: -36.817685, lng: 175.699196}
         ];
     }
+    initMap();
 }
 /*
 function getPreferences() {
@@ -20,7 +31,6 @@ function getPreferences() {
     document.getElementById("demo").innerHTML =
     obj.options[obj.selectedIndex].text;
 }*/
-
 
 
 function initMap() {
